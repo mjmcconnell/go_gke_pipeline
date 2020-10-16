4304b90178155559ea69e96df44116daffa66229
Mark McConnell <mjmcconnell.dev@gmail.com>
2020-10-16 21:13:26 +0100

    setup initial app

---
+++ apps/proxy/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+
+	"github.com/mjmcconnell/go_gke_pipeline/apps/proxy/pkg/app"
+)
+
+func main() {
+
+}
---
+++ apps/proxy/go.mod
@@ -0,0 +1,5 @@
+module github.com/mjmcconnell/go_gke_pipeline/apps/proxy
+
+go 1.15
+
+require ()
