3c688e342e1704a6fcfebe9abbc868e0b782c976
Mark McConnell <mjmcconnell.dev@gmail.com>
2020-12-10 21:05:53 +0000

    add response logging

--- apps/entrypoint/pkg/monitoring/logging.go
+++ apps/entrypoint/pkg/monitoring/logging.go
@@ -2,9 +2,36 @@ package monitoring
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	status      int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{ResponseWriter: w}
+}
+
+func (rw *loggingResponseWriter) Status() int {
+	return rw.status
+}
+
+func (rw *loggingResponseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+	rw.wroteHeader = true
+
+	return
+}
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -17,8 +44,14 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 			"referer":        r.Referer(),
 			"user_agent":     r.UserAgent(),
 			"content_length": r.ContentLength,
-		}).Info("Incoming request")
+		}).Info("Request")
 
-		next.ServeHTTP(w, r)
+		start := time.Now()
+		wrapped := wrapResponseWriter(w)
+		next.ServeHTTP(wrapped, r)
+		logger.WithFields(logrus.Fields{
+			"status":   wrapped.status,
+			"duration": time.Since(start),
+		}).Info("Response")
 	})
 }
